1041f0fe0e2af4c1e84df344409496c01fb34b6a
ckester99 <christiankester77@gmail.com>
2023-03-25 10:55:49 -0400

    updated models to match new schema

---
+++ server/models/ListedItem.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const listedItemSchema = new Schema({
+    iconURL: String,
+    title: String,
+    description: String,
+    createdAt: { Date, default: Date.now },
+    messages: [{ type: mongoose.Types.ObjectId, ref: "Message" }],
+});
+
+const listedItem = mongoose.model("ListedItem", listedItemSchema);
+
+module.exports = listedItem;
