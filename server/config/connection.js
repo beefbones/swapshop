6686f34cfa1f948314b2c32e026f65f372ce9b33
Nolan Ross <nolanross917@gmail.com>
2023-04-04 18:19:23 -0400

    heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require("mongoose");
 
-connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/swopshopDB", {
+connect(process.env.MONGODB_URI || "mongodb://127.0.0.1/swopshopDB", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
